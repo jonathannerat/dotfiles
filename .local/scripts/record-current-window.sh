f6234ed57d7b4e7fcc0cb76195944ebb7cda38f8
Jonathan Teran <jonathan.nerat@gmail.com>
2021-02-26 22:50:34 -0300

    [scripts] record-current-window: add default pulse sink as audio record

--- .local/scripts/record-current-window.sh
+++ .local/scripts/record-current-window.sh
@@ -13,8 +13,18 @@ sed -n -e "s/^ \+Absolute upper-left X: \+\([0-9]\+\).*/x=\1/p" \
        -e "s/^ \+Width: \+\([0-9]\+\).*/w=\1/p" \
        -e "s/^ \+Height: \+\([0-9]\+\).*/h=\1/p" )
 
+# select motinor of default sink by default
+# we could include a selector here in the manager
+audiosink=$(pactl info | grep -F "Default Sink:" )
+audiosink="${audiosink#*: }".monitor
+
 [ -d "$vids_folder" ] || mkdir "$vids_folder"
+
+
+
 # right now the only way to stop this is to C-c or pkill it
 # in the future we could do a screenrecord manager using dmenu that would name each recording
 # and save pids to a temporary file, that way we can stop a recording by searching its name
-exec ffmpeg -video_size "$((w-2))x$((h-2))" -f x11grab -i :0.0+$((x+4)),+$((y+4)) "$vids_folder/$(date +%F_%H%M%S_$((w-2))x$((h-2))).mp4"
+echo ffmpeg -video_size "$((w-2))x$((h-2))" -f x11grab -i :0.0+$((x+4)),+$((y+4)) -f pulse -i "$audiosink" "$vids_folder/$(date +%F_%H%M%S_$((w-2))x$((h-2))).mp4"
+exec ffmpeg -video_size "$((w-2))x$((h-2))" -f x11grab -i :0.0+$((x+4)),+$((y+4)) -f pulse -i "$audiosink" "$vids_folder/$(date +%F_%H%M%S_$((w-2))x$((h-2))).mp4"
+
