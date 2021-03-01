12d79e9feac7188d31141a086b2f76e255f7e485
Jonathan Teran <jonathan.nerat@gmail.com>
2021-02-28 21:28:54 -0300

    [scripts] search-bitwarden: send notif for password copied/not found

--- .local/scripts/search-bitwarden.sh
+++ .local/scripts/search-bitwarden.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 search="$(dmenu -p "[Bitwarden] Search:" </dev/null)"
+[ "$search" ] || exit
+
 results="$(rbw ls --fields name,user | grep -i "$search")"
 
 if [ "$(echo "$results" | wc -l )" -gt 1 ]; then
@@ -8,8 +10,13 @@ if [ "$(echo "$results" | wc -l )" -gt 1 ]; then
 	id=$(rbw ls --fields id,name,user | grep -i "$search" | head -n "$index" | tail -n1 | cut -d'	' -f1)
 	pass=$(rbw get "$id")
 else
-	id=$(rbw ls --fields id,name | grep -i "$search" | cut -d '	' -f1)
+	id=$(rbw ls --fields id,name,user | grep -i "$search" | cut -d '	' -f1)
 	pass="$(rbw get "$id")"
 fi
 
-echo "$pass" | xclip -selection clipboard
+if [ "$pass" ]; then
+	notify-send -u low "Bitwarden" "Password copied"
+	echo "$pass" | tr -d '\n' | xclip -selection clipboard
+else
+	notify-send -u low "Bitwarden" "No entry matches search term '$search'"
+fi
