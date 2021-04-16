042628351fbc057ce1f2960df7b7d2029c0ca6ed
Jonathan Teran <jonathan.nerat@gmail.com>
2021-04-15 22:46:23 -0300

    [nvim] disable lsp highlighting

--- .config/nvim/lua/init.lua
+++ .config/nvim/lua/init.lua
@@ -31,20 +31,6 @@ local on_attach = function(client, bufnr)
 	if client.resolved_capabilities.document_range_formatting then
 		buf_set_keymap("v", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
 	end
-
-	-- Set autocommands conditional on server_capabilities
-	if client.resolved_capabilities.document_highlight then
-		vim.api.nvim_exec([[
-			hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
-			hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
-			hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
-			augroup lsp_document_highlight
-				autocmd! * <buffer>
-				autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
-				autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
-			augroup END
-		]], false)
-	end
 end
 
 local servers = {
