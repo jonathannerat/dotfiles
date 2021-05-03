72ed1236833882087ae5df2654b7de2ff44ef51f
Jonathan Teran <jonathan.nerat@gmail.com>
2021-05-03 16:15:17 -0300

    [nvim] fix config-edit keymaps

--- .config/nvim/lua/my/global.lua
+++ .config/nvim/lua/my/global.lua
@@ -1,5 +1,6 @@
-
 local global   = {}
+local vim      = vim
+
 local home     = os.getenv("HOME")
 local path_sep = global.is_windows and '\\' or '/'
 local os_name  = vim.loop.os_uname().sysname
@@ -9,8 +10,9 @@ function global:load_variables()
   self.is_linux   = os_name == 'Linux'
   self.is_windows = os_name == 'Windows'
   self.vim_path    = vim.fn.stdpath('config')
-  self.cache_dir   = home .. path_sep..'.cache'..path_sep..'nvim'..path_sep
-  self.modules_dir = self.vim_path .. path_sep..'modules'
+  self.lua_dir     = self.vim_path .. path_sep .. 'lua'
+  self.cache_dir   = home .. path_sep .. '.cache' .. path_sep .. 'nvim' .. path_sep
+  self.modules_dir = self.vim_path .. path_sep .. 'modules'
   self.path_sep = path_sep
   self.home = home
   self.data_dir = string.format('%s/site/',vim.fn.stdpath('data'))
