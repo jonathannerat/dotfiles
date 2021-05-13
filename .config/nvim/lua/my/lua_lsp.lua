0aebb723e84081fe41cdd86363bf8c9b42b170a2
Jonathan Teran <jonathan.nerat@gmail.com>
2021-05-13 12:33:40 -0300

    [nvim] lua_lsp: fix indentation

--- .config/nvim/lua/my/lua_lsp.lua
+++ .config/nvim/lua/my/lua_lsp.lua
@@ -11,10 +11,10 @@ table.insert(path, "lua/?.lua")
 table.insert(path, "lua/?/init.lua")
 
 local function add(lib)
-  for _, p in pairs(vim.fn.expand(lib, false, true)) do
-    p = vim.loop.fs_realpath(p)
-    library[p] = true
-  end
+	for _, p in pairs(vim.fn.expand(lib, false, true)) do
+		p = vim.loop.fs_realpath(p)
+		library[p] = true
+	end
 end
 
 -- add runtime
@@ -29,34 +29,34 @@ add("~/.local/share/nvim/site/pack/packer/opt/*")
 add("~/.local/share/nvim/site/pack/packer/start/*")
 
 return {
-  -- delete root from workspace to make sure we don't trigger duplicate warnings
-  on_new_config = function(config, root)
-    local libs = vim.tbl_deep_extend("force", {}, library)
-    libs[root] = nil
-    config.settings.Lua.workspace.library = libs
-    return config
-  end,
-  settings = {
-    Lua = {
-      runtime = {
-	-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-	version = "LuaJIT",
-	-- Setup your lua path
-	path = path
-      },
-      completion = { callSnippet = "Both" },
-      diagnostics = {
-	-- Get the language server to recognize the `vim` global
-	globals = { "vim" }
-      },
-      workspace = {
-	-- Make the server aware of Neovim runtime files
-	library = library,
-	maxPreload = 2000,
-	preloadFileSize = 50000
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = { enable = false }
-    }
-  }
+	-- delete root from workspace to make sure we don't trigger duplicate warnings
+	on_new_config = function(config, root)
+		local libs = vim.tbl_deep_extend("force", {}, library)
+		libs[root] = nil
+		config.settings.Lua.workspace.library = libs
+		return config
+	end,
+	settings = {
+		Lua = {
+			runtime = {
+				-- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+				version = "LuaJIT",
+				-- Setup your lua path
+				path = path
+			},
+			completion = { callSnippet = "Both" },
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" }
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = library,
+				maxPreload = 2000,
+				preloadFileSize = 50000
+			},
+			-- Do not send telemetry data containing a randomized but unique identifier
+			telemetry = { enable = false }
+		}
+	}
 }
