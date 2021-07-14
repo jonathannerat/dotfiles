fe06ca41016e481408b79b2eefe97662d6b8cc4c
Jonathan Teran <jonathan.nerat@gmail.com>
2021-07-14 01:13:12 -0300

    [nvim] update plugins list

--- .config/nvim/lua/my/plugins.lua
+++ .config/nvim/lua/my/plugins.lua
@@ -5,21 +5,20 @@ local packages = {
 	'cohama/lexima.vim',
 	'editorconfig/editorconfig-vim',
 	'folke/tokyonight.nvim',
-	'kdheepak/lazygit.nvim',
 	'lambdalisue/suda.vim',
 	'lervag/vimtex',
 	'mcchrish/nnn.vim',
-	'mhinz/vim-startify',
 	'nvim-treesitter/nvim-treesitter-textobjects',
+	'rafamadriz/neon',
 	'ray-x/lsp_signature.nvim',
 	'sainnhe/gruvbox-material',
 	'tpope/vim-commentary',
 	'tpope/vim-fugitive',
-	'tpope/vim-obsession',
 	'tpope/vim-surround',
 	'vhyrro/tree-sitter-norg',
 	'vim-pandoc/vim-pandoc',
 
+	[ 'wbthomason/packer.nvim' ]   = { opt = true },
 	['arrufat/vala.vim']           = { ft = 'vala' },
 	['cakebaker/scss-syntax.vim']  = { ft = 'scss' },
 	['cespare/vim-toml']           = { ft = 'toml' },
@@ -34,87 +33,47 @@ local packages = {
 	['vim-pandoc/vim-pandoc-syntax'] = { ft = 'pandoc' },
 
 	['neovim/nvim-lspconfig'] = {
+		config = function() require'my.plugins.lspconfig'.config() end
+	},
+
+	['folke/todo-comments.nvim'] = {
+		requires = 'nvim-lua/plenary.nvim',
+		config = function () require'todo-comments'.setup {} end
+	},
+
+	['andweeb/presence.nvim'] = {
+		config = function() require'presence':setup() end
+	},
+
+	['TimUntersberger/neogit'] = {
+		requires = 'nvim-lua/plenary.nvim',
+		config = function() require'neogit'.setup{} end
+	},
+
+	['sunjon/shade.nvim'] = {
 		config = function()
-			local lspconfig = require'lspconfig'
-			local m = require'my.util.mapper'
-			local on_attach = function(client, bufnr)
-				local mappings = {
-					['n|ns|gD']         = m.cmd('lua vim.lsp.buf.declaration()'),
-					['n|ns|gd']         = m.cmd('lua vim.lsp.buf.definition()'),
-					['n|ns|K']          = m.cmd('lua vim.lsp.buf.hover()'),
-					['n|ns|gi']         = m.cmd('lua vim.lsp.buf.implementation()'),
-					['n|ns|<C-k>']      = m.cmd('lua vim.lsp.buf.signature_help()'),
-					['n|ns|<leader>wa'] = m.cmd('lua vim.lsp.buf.add_workspace_folder()'),
-					['n|ns|<leader>wr'] = m.cmd('lua vim.lsp.buf.remove_workspace_folder()'),
-					['n|ns|<leader>wl'] = m.cmd('lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))'),
-					['n|ns|<leader>D']  = m.cmd('lua vim.lsp.buf.type_definition()'),
-					['n|ns|<leader>rn'] = m.cmd('lua vim.lsp.buf.rename()'),
-					['n|ns|<leader>ca'] = m.cmd('lua vim.lsp.buf.code_action()'),
-					['n|ns|gr']         = m.cmd('lua vim.lsp.buf.references()'),
-					['n|ns|<leader>e']  = m.cmd('lua vim.lsp.diagnostic.show_line_diagnostics()'),
-					['n|ns|[d']         = m.cmd('lua vim.lsp.diagnostic.goto_prev()'),
-					['n|ns|]d' ]        = m.cmd('lua vim.lsp.diagnostic.goto_next()'),
-					['n|ns|<leader>dd'] = m.cmd('lua vim.lsp.diagnostic.set_loclist()'),
-				}
-
-				if client.resolved_capabilities.document_formatting then
-					mappings['n|ns|<leader>f'] = m.cmd('lua vim.lsp.buf.formatting()')
-				end
-
-				if client.resolved_capabilities.document_range_formatting then
-					mappings['v|ns|<leader>f'] = m.cmd('lua vim.lsp.buf.range_formatting()')
-				end
-
-				m.bind(mappings, bufnr)
-
-				require'lsp_signature'.on_attach {
-					bind = true,
-					doc_lines = 2,
-					hint_enable = false,
-					handler_opts = {
-						border = 'single',
-					}
-				}
-			end
-
-			local lsp_servers = {
-				tsserver = {},
-				cssls = {},
-				vimls = {},
-				ccls = {},
-				bashls = {},
-				pyls = {},
-				rust_analyzer = {},
-				solargraph = {},
-				sumneko_lua = require'my.lua_lsp',
-				jsonls = {
-					cmd = { 'json-languageserver', '--stdio' }
-				},
-				texlab = {},
+			require'shade'.setup {
+				overlay_opacity = 50,
+				opacity_step = 5,
 			}
+		end
+	},
 
-			lsp_servers.sumneko_lua.cmd = {
-				'/home/jonathan/.local/src/lua-language-server/bin/Linux/lua-language-server',
-				'-E',
-				'/home/jonathan/.local/src/lua-language-server/main.lua'
+	['L3MON4D3/LuaSnip'] = {
+		config = function() require'my.snippets'.setup() end
+	},
+
+	['norcalli/nvim-colorizer.lua'] = {
+		config = function()
+			require'colorizer'.setup(nil, { css=true })
+		end
+	},
+
+	['folke/trouble.nvim'] = {
+		config = function()
+			require'trouble'.setup{
+				icons = false
 			}
-
-			local capabilities = vim.lsp.protocol.make_client_capabilities()
-			capabilities.textDocument.completion.completionItem.snippetSupport = true
-			capabilities.textDocument.completion.completionItem.resolveSupport = {
-				properties = {
-					'documentation',
-					'detail',
-					'additionalTextEdits',
-				}
-			}
-
-			for lsp, config in pairs(lsp_servers) do
-				if not config.on_attach    then config.on_attach    = on_attach end
-				if not config.capabilities then config.capabilities = capabilities end
-
-				lspconfig[lsp].setup(config)
-			end
 		end
 	},
 
@@ -135,6 +94,7 @@ local packages = {
 					calc = true,
 					nvim_lsp = true,
 					luasnip = true,
+					neorg = true,
 				}
 			}
 		end
@@ -143,6 +103,16 @@ local packages = {
 	['nvim-treesitter/nvim-treesitter'] = {
 		run = ':TSUpdate',
 		config = function()
+			local parser_configs = require'nvim-treesitter.parsers'.get_parser_configs()
+
+			parser_configs.norg = {
+				install_info = {
+					url = 'https://github.com/vhyrro/tree-sitter-norg',
+					files = { 'src/parser.c' },
+					branch = 'main'
+				}
+			}
+
 			require'nvim-treesitter.configs'.setup {
 				ensure_installed = "maintained",
 				-- Modules
@@ -166,7 +136,6 @@ local packages = {
 	},
 
 	['hoob3rt/lualine.nvim'] = {
-		requires = { {'ryanoasis/vim-devicons', opt = true}, {'tpope/vim-fugitive', opt = true} },
 		config = function()
 			require'lualine'.setup {
 				sections = {
@@ -193,31 +162,9 @@ local packages = {
 		end
 	},
 
-	['folke/trouble.nvim'] = {
-		config = function()
-			require'trouble'.setup{
-				icons = false
-			}
-		end
-	},
-
 	['nvim-telescope/telescope.nvim'] = {
 		requires = { 'nvim-lua/popup.nvim', 'nvim-lua/plenary.nvim' },
-		config = function()
-			require'my.plugins.telescope'.config()
-		end
-	},
-
-	['L3MON4D3/LuaSnip'] = {
-		config = function()
-			require'my.snippets'.setup()
-		end
-	},
-
-	['norcalli/nvim-colorizer.lua'] = {
-		config = function()
-			require'colorizer'.setup(nil, { css=true })
-		end
+		config = function() require'my.plugins.telescope'.config() end
 	},
 
 	['vhyrro/neorg'] = {
@@ -225,6 +172,9 @@ local packages = {
 			require'neorg'.setup {
 				load = {
 					['core.defaults'] = {},
+					['core.keybinds'] = {
+						config = { default_keybinds = true }
+					},
 					['core.norg.concealer'] = {},
 					['core.norg.dirman'] = {
 						config = { workspaces = { notes = '~/docs/notes' } }
@@ -241,9 +191,7 @@ local packages = {
 	},
 }
 
-function M.setup()
-	require'packer'.startup(function(use)
-		use { 'wbthomason/packer.nvim', opt = true }
+local function packer_setup(use)
 		for k, package in pairs(packages) do
 			if type(k) == 'number' then
 				package = { package }
@@ -253,7 +201,23 @@ function M.setup()
 
 			use(package)
 		end
-	end)
+end
+
+function M.setup()
+	require'packer'.startup {
+		packer_setup,
+		config = {
+			display = {
+				open_fn = function()
+					return require'packer.util'.float { border = 'single' }
+				end
+			},
+			profile = {
+				enabled = true,
+				threshold = 1,
+			}
+		}
+	}
 end
 
 return M
