aa53fbc96bc94a991279a81a25ed1bbd521d0ee6
Jonathan Teran <jonathan.nerat@gmail.com>
2021-07-22 11:01:41 -0300

    [nvim] add more snippets

--- .config/nvim/lua/my/snippets/html.lua
+++ .config/nvim/lua/my/snippets/html.lua
@@ -8,10 +8,8 @@ local i = ls.insert_node
 local s = ls.snippet_node
 local t = ls.text_node
 
-return {
-	ls.parser.parse_snippet(
-		'skel',
-		[[<!DOCTYPE html>
+local parseable_snippets = {
+	skel = [[<!DOCTYPE html>
 <html>
 <head>
 	<title>${1:Title}</title>
@@ -20,7 +18,10 @@ return {
 </head>
 <body>
 	${0:}
-</body>]]),
+</body>]]
+}
+
+local snippets = {
 	S('script', {
 		t '<script',
 		c (1, {
@@ -49,3 +50,9 @@ return {
 		end, {}, i(0))
 	})
 }
+
+for trigger, snippet_def in ipairs(parseable_snippets) do
+	snippets[#snippets+1] = ls.parser.parse_snippet(trigger, snippet_def)
+end
+
+return snippets
--- .config/nvim/lua/my/snippets/tex.lua
+++ .config/nvim/lua/my/snippets/tex.lua
@@ -5,8 +5,25 @@ local f = ls.function_node
 local t = ls.text_node
 local i = ls.insert_node
 
+local parseable_snippets = {
+	skel = [[\documentclass[a4paper,10pt]{article}
 
-return {
+\usepackage[utf8]{inputenc}
+\usepackage[spanish]{babel}
+\usepackage{graphix}
+\usepackage{float}
+
+\author{${1:Jonathan Teran}}
+\title{${2:Document Title}}
+\date{}
+
+\begin{document}
+$0
+\end{document}
+]]
+}
+
+local snippets = {
 	S('begin', {
 		t '\\begin{',
 		i(1),
@@ -15,7 +32,11 @@ return {
 		t { '', '\\end{' },
 		f(u.copy, 1),
 		t '}'
-
-
 	})
 }
+
+for trigger, snippet_def in pairs(parseable_snippets) do
+	snippets[#snippets+1] = ls.parser.parse_snippet(trigger, snippet_def)
+end
+
+return snippets
