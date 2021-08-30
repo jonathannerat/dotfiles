295e858c3d3f5b146ba077e7eebaf1fcc2c10138
Jonathan Teran <jonathan.nerat@gmail.com>
2021-08-30 16:35:18 -0300

    [zathura] add script to extract images from pdf

---
+++ .local/scripts/pdf-current-images.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+# extracts images from a pdf's current page. needs the file and current page to work
+
+file="$1" # path to the file
+page="$2" # current page
+dest="$3" # optional destination directory for the images
+
+tmpdir="$(mktemp -d)"
+template="${file##*/}-page_${page}"
+
+cleanup() {
+	[ -d "$tmpdir" ] && rm -r "$tmpdir"
+}
+
+trap cleanup EXIT
+
+pdfimages -f "$page" -l "$page" -png "$file" "$tmpdir/$template"
+
+if [ "$dest" ]; then
+	env sxiv SXIV_SAVE_DIR="$dest" "$tmpdir"
+else
+	sxiv "$tmpdir"
+fi
