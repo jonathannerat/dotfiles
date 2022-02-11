bc6c010889a4d2b96b64550225e86bde45104a29
Jonathan Teran <jonathan.nerat@gmail.com>
2022-02-11 11:04:36 -0300

    update search-bitwarden script

--- .local/scripts/search-bitwarden.sh
+++ .local/scripts/search-bitwarden.sh
@@ -1,22 +1,10 @@
 #!/bin/sh
 
-search="$(dmenu -p "[Bitwarden] Search:" </dev/null)"
-[ "$search" ] || exit
+idx="$(rbw ls --fields=name,user\
+	| while IFS='	' read -r name user; do echo "$name${user:+ ($user)}"; done\
+	| dmenu -ix -i "$@")"
 
-results="$(rbw ls --fields name,user | grep -i "$search")"
-
-if [ "$(echo "$results" | wc -l )" -gt 1 ]; then
-	index=$(echo "$results" | dmenu -vp 0 -hp 0 -I -c -B -l 5)
-	id=$(rbw ls --fields id,name,user | grep -i "$search" | head -n "$((index+1))" | tail -n1 | cut -d'	' -f1)
-	pass=$(rbw get "$id")
-else
-	id=$(rbw ls --fields id,name,user | grep -i "$search" | cut -d '	' -f1)
-	pass="$(rbw get "$id")"
-fi
-
-if [ "$pass" ]; then
-	notify-send -u low "Bitwarden" "Password copied"
-	echo "$pass" | tr -d '\n' | xclip -selection clipboard
-else
-	notify-send -u low "Bitwarden" "No entry matches search term '$search'"
+if [ "$idx" ]; then
+	id="$(rbw ls --fields=id 2>/dev/null | head -n$((idx+1)) | tail -n1)"
+	rbw get "$id" | tr -d '\n' | xclip -selection clipboard
 fi
