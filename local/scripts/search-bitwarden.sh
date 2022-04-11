14e86b364f5bc2822b0a1f0af48e736b45ea84b4
Jonathan Teran <jonathan.nerat@gmail.com>
2022-04-11 10:21:02 -0300

    search-bitwarden: fix strange window when nothing is selected

--- local/scripts/search-bitwarden.sh
+++ local/scripts/search-bitwarden.sh
@@ -4,7 +4,7 @@ idx="$(rbw ls --fields=name,user\
 	| while IFS='	' read -r name user; do echo "$name${user:+ ($user)}"; done\
 	| dmenu -ix -i "$@")"
 
-if [ "$idx" ]; then
+if [ -n "$idx" ]; then
 	id="$(rbw ls --fields=id 2>/dev/null | head -n$((idx+1)) | tail -n1)"
 	rbw get "$id" | tr -d '\n' | xclip -selection clipboard
 fi
