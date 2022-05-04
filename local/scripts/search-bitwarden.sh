9e9365eb3e00fb8367a48e573cc9b81ea815d91a
Jonathan Teran <jonathan.nerat@gmail.com>
2022-05-04 20:54:06 -0300

    scripts: update search-bitwarden.sh

--- local/scripts/search-bitwarden.sh
+++ local/scripts/search-bitwarden.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-idx="$(rbw ls --fields=name,user\
-	| while IFS='	' read -r name user; do echo "$name${user:+ ($user)}"; done\
-	| dmenu -ix -i "$@")"
+if rbw unlock; then
+  idx="$(rbw ls --fields=name,user\
+       | while IFS='	' read -r name user; do echo "$name${user:+ ($user)}"; done\
+       | dmenu -ix -i "$@")"
 
-if [ -n "$idx" ]; then
-	id="$(rbw ls --fields=id 2>/dev/null | head -n$((idx+1)) | tail -n1)"
-	rbw get "$id" | tr -d '\n' | xclip -selection clipboard
+  if [ $idx ]; then
+    id="$(rbw ls --fields=id 2>/dev/null | head -n$((idx + 1)) | tail -n1)"
+    rbw get "$id" | tr -d '\n' | xclip -selection clipboard
+  fi
 fi
