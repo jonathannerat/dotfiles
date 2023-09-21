7ccdaf6fff46afe54854a95dcea639416233f05d
Jonathan Teran <jonathan.nerat@gmail.com>
2023-09-21 13:00:55 -0300

    delete unused scripts

--- local/scripts/tmux-select-buffer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-wid="0x$( (echo 'obase=16;' ; xdotool getwindowfocus) | bc )"
-buffer="$(tmux list-buffers -F "#{buffer_name}: #{buffer_sample}" | dmenu -W "$wid" -vp 0 -hp 0 -l 5 | cut -d: -f1)"
-
-tmux show-buffer -b "$buffer"
